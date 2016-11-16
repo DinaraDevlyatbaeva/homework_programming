8e58cc3aad13dd3c6a4e84bb35f5d6f2fa5f315c
Dinarida <ms.devlyatbaeva@mail.ru>
2016-11-17 00:01:29 +0300

    Add files via upload

---
+++ programm3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main(void){
+	int num, res, deg;
+	scanf("%d %d", &deg, &num);
+    if (deg >= 0){
+        res = pow (num, deg);
+        printf("%d", res);
+    }
+    else{
+        printf("Net resheniya");
+    }
+	return 0;
+}
+
+
