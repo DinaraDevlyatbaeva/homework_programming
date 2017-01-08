acdc5a11885e34a0c3d0200916a2c02cda34152a
Dinarida <ms.devlyatbaeva@mail.ru>
2017-01-09 00:57:52 +0300

    Add files via upload

---
+++ programm9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+	int n, a, b, c, v;
+	printf("Введите n-ую секунду суток:\n"); 
+	scanf("%d", &n);
+	b = n / 3600;
+	c = n % 3600;
+	a = c / 60;
+	v = c % 60;
+	printf("%d часов, %d минут и %d секунд", b, a, v);
+	return 0;
+}
+	
+	
